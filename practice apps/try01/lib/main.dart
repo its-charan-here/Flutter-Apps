a12411f1ad494fb157c6bacbd2efc3d4abaf185b
Vignesh Charan <vigneshcharan26@gmail.com>
2020-11-17 23:20:43 +0530

    stateful widget implemeted with chaning states on button click

--- practice apps/try01/lib/main.dart
+++ practice apps/try01/lib/main.dart
@@ -1,13 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(
+    MyApp()); // this is a shorthand technique used to call the main function
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+    // print('Answer Chosen!');
+  }
+
+  @override // to make it clear that u r deliberately overriding the given class
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite colour?',
+      'What\'s your favourite animal?',
+    ];
     return MaterialApp(
-        home: Text(
-            'Hello!')); // we pass named arguments hence the argument should be via name not direct data
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //...
+                print('Answer 3 chosen!');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
