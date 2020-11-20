15a68233539d854428da9a06470d6c82f9c0d2f1
Vignesh Charan <vigneshcharan26@gmail.com>
2020-11-20 16:45:10 +0530

    E-Transaction app

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -1,35 +1,45 @@
 import 'package:flutter/material.dart';
 
+import './widgets/user_transactions.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Expense Logger',
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('E-Logger'),
       ),
       body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             width: double.infinity,
             child: Card(
+              // card is the child of the parent container which clearly sets the size
               color: Colors.blue,
               child: Text('Chart!'),
               elevation: 5,
             ),
           ),
-          Card(child: Text('List of Tx!'))
+          UserTransactions()
         ],
       ),
     );
